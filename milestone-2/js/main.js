222523efa0b99fc15a71f1bf82ebc8e1c833d67e
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-16 20:55:09 +0100

    Profilo contatto attivo e conversazioni dinamici

--- milestone-2/js/main.js
+++ milestone-2/js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
+            activeConversation: 0,
             contacts: [
                 {
                     name: 'Michele',
